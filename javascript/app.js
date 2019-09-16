58fdb2c95ac9502a12e17aa2346ddf5a65a114ae
bhargava.yadavalli <bhargava.yadavalli@mountblue.io>
2019-09-16 23:19:23 +0530

    feat : pusher and database integration

--- javascript/app.js
+++ javascript/app.js
@@ -30,4 +30,110 @@ function openChannel(channel_id) {
     })
 }
 
+function getMessages(channel_id){
+    fetch("http://0.0.0.0:5000/messages")
+    .then(response => response.json())
+    .then(json_db_object => json_db_object['resources'])
+    .then(messages => {
+        inbox_node = document.getElementById("inbox");
+        inbox_node.innerHTML = "";
+        messages.forEach(message => {
+            if(message['channel_id'] == channel_id) {
+                message_node = document.createElement("li");
+                user_node = document.createElement("b");
+                message_text = document.createTextNode(message['text']);
+                user_name = document.createTextNode(message['username']+" : ");
+                user_node.appendChild(user_name);
+                message_node.appendChild(user_node);
+                message_node.appendChild(message_text);
+                inbox_node.appendChild(message_node);
+            }
+        })
+    })
+}
 
+function getChannels(){
+    fetch("http://0.0.0.0:5000/channels")
+    .then(response => response.json())
+    .then(json_db_object => json_db_object['resources'])
+    .then(channels => {
+        inner_html = "";
+        channels.forEach(channel => {
+            inner_html += '<li><button id="' + channel['id'] + '" class="channel" onClick="openChannel(this.id);">' + channel['name'] + '</button></li>';
+        }
+        )
+        channels_list = document.getElementById('channels');
+        channels_list.innerHTML = "";
+        channels_list.innerHTML = inner_html;            
+    });
+}
+
+function sendMessage() {
+    let message = document.getElementById('message').value;
+    let user_name = document.getElementById("user").name;
+    let channel_id = document.getElementById("channel-name").name
+    if(channel_id!=null && message != ""){
+        fetch("http://0.0.0.0:5000/messages/", {
+            method: 'POST',
+            body: JSON.stringify(
+                { 
+                    'username' : user_name,
+                    'text' : message,
+                    'channel_id' : channel_id
+                })
+        })
+        .then(() => getMessages(channel_id))
+        .catch(error => console.log(error))
+        broadcastMessage(channel_id, user_name, message);
+    }
+    document.getElementById('message').value = "";
+}
+
+function broadcastMessage(channel_id, user_name, message){
+        fetch("http://0.0.0.0:5000/broadcast", {
+        method: 'POST',
+        body: JSON.stringify(
+            { 
+                'username' : user_name,
+                'text' : message,
+                'channel_id' : channel_id
+            }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+function addChannel() {
+    let channel_name = document.getElementById('new-channel').value;
+    if(channel_name != ""){
+        fetch("http://0.0.0.0:5000/channels/", {
+            method: 'POST',
+            body: JSON.stringify(
+                { 
+                    'name' : channel_name,
+                })
+        })
+        .then(() => getChannels());
+        broadcastChannel(channel_name);
+    }
+    document.getElementById('new-channel').value = "";
+}
+
+function broadcastChannel(channel_name) {
+    fetch("http://0.0.0.0:5000/broadcast", {
+            method: 'POST',
+            body: JSON.stringify(
+                { 
+                    'name' : channel_name,
+                }),
+            headers: {
+                    'Content-Type': 'application/json'
+                }
+        })
+}
+
+document.getElementById('message').value = "";
+document.getElementById('user-name').value = "";
+document.getElementById("login").addEventListener("click", login);
+document.getElementById("send").addEventListener("click", sendMessage);
+document.getElementById("add-channel").addEventListener("click", addChannel);
